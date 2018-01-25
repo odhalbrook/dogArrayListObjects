6502ac9698cda2aa890ecd4317d299b0e8a06e9a
owen.halbrook <owen.halbrook@MU-C1-12.mullen.pvt.k12.co.us>
2018-01-25 08:30:40 -0700

    Commit added dogs

--- ArrayListOfDogs/src/DogKennel.java
+++ ArrayListOfDogs/src/DogKennel.java
@@ -5,12 +5,23 @@ public class DogKennel
 		public static void main(String[] args)
 			{
 				fillArray();
+				printDog();
 			}
+
 
 		private static void fillArray()
 			{
 				kennel.add(new Dog("George", "Boston Terrier", 3, 21));
+				kennel.add(new Dog("Larry", "French Bulldog", 2, 26));
+				kennel.add(new Dog("Luke", "Pitbull", 1, 14));
+				kennel.add(new Dog("John", "Beagle", 7, 15));
+				kennel.add(new Dog("James", "Pug", 3, 28));
 				
 			}
 
+		private static void printDog()
+			{
+				// TODO Auto-generated method stub
+				
+			}
 	}
